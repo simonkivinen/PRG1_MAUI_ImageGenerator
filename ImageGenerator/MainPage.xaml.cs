045e7a17882de3da984c1fcd1f16c98468e0b00e
simonkivinen <kivinen.simon@gmail.com>
2025-10-15 13:44:40 +0200

    added more images
    I have added 5 more images and the code for them

--- ImageGenerator/MainPage.xaml.cs
+++ ImageGenerator/MainPage.xaml.cs
@@ -1,19 +1,34 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace ImageGenerator
 {
     public partial class MainPage : ContentPage
     {
-        static private bool _isFavorite;
+        private class ImageItem
+        {
+            public string FileName { get; set; }
+            public string Title { get; set; }
+        }
 
-        private Dictionary<string, string> ImageList = new()
-            {
-                {"image1", "Man" },
-                {"image2", "Bird" },
-                {"image3", "Big cat" },
-                {"image4", "Autumn road" },
-                {"image5", "Flowergirl" }
-            };
+        private readonly List<ImageItem> _images = new()
+        {
+            new ImageItem { FileName = "image1", Title = "Man" },
+            new ImageItem { FileName = "image2", Title = "Bird" },
+            new ImageItem { FileName = "image3", Title = "Big Cat" },
+            new ImageItem { FileName = "image4", Title = "Autumn road" },
+            new ImageItem { FileName = "image5", Title = "Flowergirl" },
+            new ImageItem { FileName = "image6", Title = "Far you have comed warior" },
+            new ImageItem { FileName = "image7", Title = "Robots in descise" },
+            new ImageItem { FileName = "image8", Title ="Rust"},
+            new ImageItem { FileName = "image9", Title = "coding joke"},
+            new ImageItem { FileName = "image10", Title ="Feer will keep them inline"},
+        };
+
+        private readonly List<string> _favoriteList = new();
+        private readonly Stack<string> _recentFavorites = new();
+        private string _currentImageKey;
 
 
         private Random random = new();
@@ -30,38 +45,73 @@ namespace ImageGenerator
 
         private void ShowImageAndText()
         {
-            var pairs = ImageList.ElementAt(random.Next(ImageList.Count));
+            var item = _images[random.Next(_images.Count)];
+            _currentImageKey = item.FileName;
 
-            Debug.WriteLine(pairs.Key + ": " + pairs.Value); // för testning i Output
-
-            string showKey = GetImageFileEnding(pairs.Key); // detta då Windows, men inte till exempel Android, kräver filändelse
+            string showKey = GetImageFileEnding(item.FileName);
 
             ShowGallery.Source = showKey;
+            ImageText.Text = item.Title;
 
-            ImageText.Text = pairs.Value;
+            UpdatefavoriteIcon();
         }
 
         private string GetImageFileEnding(string imageKey)
         {
-            #if WINDOWS
+#if WINDOWS
             return imageKey + ".jpg";
-            #else
+#else
             return imageKey;
-            #endif
+#endif
         }
+
 
         private void OnFavoriteClicked(object sender, EventArgs e)
         {
-            _isFavorite = !_isFavorite;
+            if (string.IsNullOrEmpty(_currentImageKey))
+                return;
 
-            if (_isFavorite)
+            bool isFavorite = _favoriteList.Contains(_currentImageKey);
+
+            if (isFavorite)
             {
+                _favoriteList.Remove(_currentImageKey);
+                FavoriteButton.Source = new FontImageSource
+                {
+                    Glyph = "\ue87e",
+                    FontFamily = "MaterialIcons",
+                    Size = 32,
+                    Color = Colors.Grey
+                };
+            }
+            else
+            {
+                _favoriteList.Add(_currentImageKey);
+                _recentFavorites.Push(_currentImageKey);
                 FavoriteButton.Source = new FontImageSource
                 {
                     Glyph = "\ue87d",
                     FontFamily = "MaterialIcons",
                     Size = 32,
                     Color = Colors.Red
+                };
+                Debug.WriteLine($"Senast Favorit: {_recentFavorites.Peek()}");
+            }
+        }
+        private void UpdatefavoriteIcon()
+        {
+            if (string.IsNullOrEmpty(_currentImageKey))
+                return;
+            bool isFavorite = _favoriteList.Contains(_currentImageKey);
+
+            if (isFavorite)
+            {
+                FavoriteButton.Source = new FontImageSource
+                {
+                    Glyph = isFavorite ? "\ue87d" : "\ue87e",
+                    FontFamily = "MaterialIcons",
+                    Size = 32,
+                    Color = isFavorite ? Colors.Red : Colors.Grey
                 };
             }
             else
@@ -71,8 +121,17 @@ namespace ImageGenerator
                     Glyph = "\ue87e",
                     FontFamily = "MaterialIcons",
                     Size = 32,
-                    Color = Colors.Gray
+                    Color = Colors.Grey
                 };
+            }
+        }
+
+        private void ShowLastFavorite()
+        {
+            if (_recentFavorites.Count > 0)
+            {
+                string lastFav = _recentFavorites.Peek();
+                Debug.WriteLine($"Senast Favorit: {lastFav}");
             }
         }
     }
